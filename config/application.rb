1d3ab8260358ce7e8ee2082b2ef43671959a25a3
isonoryo <ljiye2422@gmail.com>
2019-12-13 12:34:03 +0900

    css,js,helperの自動生成抑制処理

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,11 @@ module Isostagram
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helpers false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
